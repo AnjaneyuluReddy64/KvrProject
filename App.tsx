b4b7cd5d8f826a9479cab5e9212d7c0ff1ed0d16
Anjaneyulu Reddy <anjaneyulureddy64@gmail.com>
2024-12-24 12:11:53 +0530

    Old Project files

--- App.tsx
+++ App.tsx
@@ -1,10 +1,58 @@
-import {View, Text} from 'react-native';
+import {View, Text, SafeAreaView} from 'react-native';
 import React from 'react';
+import Variables from './src/ModuleOne/Variables';
+import DataTypes from './src/ModuleOne/DataTypes';
+import Function from './src/ModuleOne/Function';
+import Hooks from './src/ModuleOne/Hooks';
+import Chaining from './src/ModuleOne/Chaining';
+import CounterApp from './src/ModuleOne/CounterApp';
+import Inline from './src/ModuleTwo/CssTypes/Inline';
+import Internal from './src/ModuleTwo/CssTypes/Internal';
+import External from './src/ModuleTwo/CssTypes/External';
+import ScrollViewComponent from './src/ModuleTwo/ScrollViewComponent';
+import FlatListComponent from './src/ModuleTwo/FlatListComponent';
+import FlatlistInsideScrollView from './src/ModuleTwo/FlatlistInsideScrollView';
+import ActivityIndicatorComponent from './src/ModuleTwo/ActivityIndicatorComponent';
+import SortComponent from './src/ModuleOne/SortComponent';
+import FliterComponent from './src/ModuleOne/FliterComponent';
+import ModelComponent from './src/ModuleTwo/ModelComponent';
+import ImageComponent from './src/ModuleTwo/ImageComponent';
+import BackgroundImage from './src/ModuleTwo/BackgroundImage';
+import ModalImageCoomponent from './src/ModuleTwo/ModalImageCoomponent';
+import InputComponent from './src/ModuleTwo/InputComponent';
+import AudioComponent from './src/ModuleTwo/AudioComponent';
+import SwitchandChecbox from './src/ModuleTwo/SwitchandChecbox';
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
+    <View style={{flex: 1}}>
+      {/* ModuleOne */}
+      {/* <DataTypes /> */}
+      {/* <Variables /> */}
+      {/* <Function /> */}
+      {/* <Hooks /> */}
+      {/* <Chaining /> */}
+      {/* <Conditons /> */}
+      {/* <CounterApp /> */}
+      {/* <SortComponent /> */}
+      {/* <FliterComponent /> */}
+      {/* ModuleTwo */}
+      {/* <Inline /> */}
+      {/* <Internal /> */}
+      {/* <External /> */}
+      {/* <ResponsiveCss /> */}
+      {/* <ScrollViewComponent /> */}
+      {/* <FlatListComponent /> */}
+      {/* <FlatlistInsideScrollView /> */}
+      {/* <ActivityIndicatorComponent /> */}
+      {/* <ModelComponent /> */}
+      {/* <ImageComponent /> */}
+      {/* <BackgroundImage /> */}
+      {/* <ModalImageCoomponent /> */}
+      {/* <InputComponent /> */}
+      {/* <AudioComponent /> */}
+
+      <SwitchandChecbox />
     </View>
   );
 };
